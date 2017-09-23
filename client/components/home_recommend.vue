dd7032a0f39f4f283f3591c8a3e64336db38894f
taoyage <tougakaku@gmail.com>
2017-09-24 01:31:50 +0800

    fix bug

--- client/components/home_recommend.vue
+++ client/components/home_recommend.vue
@@ -1,263 +1,266 @@
 <template>
-    <section class="channel-h5" id="recommend">
-        <div class="channel-5__header">
-            <p class="chanel-h5__title">
-                重磅推荐<i>推</i>
-            </p>
-            <div class="tab">
-                <a href="javascript:" class="tab__on">男</a>
-                <a href="javascript:">女</a>
+  <section class="channel-h5" id="recommend">
+    <div class="channel-5__header">
+      <p class="chanel-h5__title">
+        重磅推荐<i>推</i>
+      </p>
+      <div class="tab">
+        <a href="" class="tab__on">男</a>
+        <a href="">女</a>
+      </div>
+    </div>
+    <div>
+      <ul class="list-h5">
+        <li v-for="(item,index) in RecommendData" v-if="index === 0">
+          <div class="book-h5">
+            <div class="book-h5__cover">
+              <img alt="title" v-bind:src="item.cover">
+              <p class="book-h5__finish" v-if="item.finish">完结</p>
+              <p class="book-h5__finish" v-else>未完结</p>
             </div>
-        </div>
-        <div>
-            <ul class="list-h5">
-                <li v-for="(item,index) in RecommendData" v-if="index === 0">
-                    <div class="book-h5">
-                        <div class="book-h5__cover">
-                            <img alt="title" v-bind:src="item.cover">
-                            <p class="book-h5__finish" v-if="item.finish">完结</p>
-                            <p class="book-h5__finish" v-else>未完结</p>
-                        </div>
-                        <div class="book-h5__info">
-                            <p class="book-h5__title">{{item.title}}</p>
-                            <p class="book-h5__author">{{item.authors}}</p>
-                            <p class="book-h5__summary">{{item.summary}}</p>
-                            <div class="book-h5__wrap">
-                                <div class="book-h5__tag" v-for="tags in item.tags">{{tags}}</div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li v-for="(item,index) in RecommendData" v-if="index > 0 && index < 5">
-                    <div class="book-h5 book-h5__no-img">
-                        <span class="book-h5_no-img__order">0{{index + 1}}</span>
-                        <div class="book-h5_no-img__info">
-                            <p class="book-h5_no-img__title">
-                                {{item.title}}
-                                <i class="book-h5_no-img__author">
+            <div class="book-h5__info">
+              <p class="book-h5__title">{{item.title}}</p>
+              <p class="book-h5__author">{{item.authors}}</p>
+              <p class="book-h5__summary">{{item.summary}}</p>
+              <div class="book-h5__wrap">
+                <div class="book-h5__tag" v-for="tags in item.tags">{{tags}}</div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li v-for="(item,index) in RecommendData" v-if="index > 0 && index < 5">
+          <div class="book-h5 book-h5__no-img">
+            <span class="book-h5_no-img__order">0{{index + 1}}</span>
+            <div class="book-h5_no-img__info">
+              <p class="book-h5_no-img__title">
+                {{item.title}}
+                <i class="book-h5_no-img__author">
                             <span class="author">{{item.authors}}</span>
                         </i>
-                            </p>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-            <div class="channel-h5__footer_next">
-                <a href="javascript:">换一换</a>
-                <a href="/male">男生频道</a>
+              </p>
             </div>
-    </section>
+          </div>
+        </li>
+      </ul>
+      <div class="channel-h5__footer_next">
+        <a href="javascript:">换一换</a>
+        <a href="/male">男生频道</a>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
-    export default {
-       props:['RecommendData']
-    }
+export default {
+  props: ['RecommendData']
+}
+
 </script>
 <style scoped>
-    .channel-h5 {
-        background: #fff;
-        border-bottom: 10px solid #f5f5f5;
-        position: relative;
-    }
-    
-    .channel-5__header {
-        padding: 15px 13px 14px 13px;
-        border-bottom: 1px solid #f0f0f0;
-    }
-    
-    .chanel-h5__title {
-        position: relative;
-        font: bold 13px/13px a;
-        color: rgba(0, 0, 0, 0.9);
-    }
-    
-    .chanel-h5__title > i {
-        position: absolute;
-        margin: -1px 0 0 5px;
-        padding: 3px 5px 0 5px;
-        font: 9px/9px a;
-        color: #fff;
-        background: #53ac7d;
-        border-radius: 1px;
-    }
-    
-    .tab {
-        position: absolute;
-        right: 13px;
-        top: 9px;
-    }
-    
-    .tab a {
-        position: relative;
-        padding: 16px 7px;
-        font: 12px/12px a;
-        color: #999;
-    }
-    
-    .tab a:after {
-        content: '';
-        position: absolute;
-        top: 16px;
-        bottom: 16px;
-        right: -1px;
-        width: 1px;
-        border-right: 1px solid #ccc;
-    }
-    
-    .tab a:last-child {
-        padding-right: 0px;
-    }
-    
-    .tab a:last-child:after {
-        display: none;
-    }
-    
-    .tab__on {
-        color: #528ae8 !important;
-    }
-    
-    .list-h5 {
-        padding: 0 13px;
-    }
-    
-    .list-h5 li {
-        padding: 17px 0;
-        border-bottom: 1px solid #f0f0f0;
-    }
-    
-    .list-h5 li:last-child {
-        border: none;
-    }
-    
-    .book-h5 {
-        overflow: hidden;
-    }
-    
-    .book-h5__cover {
-        float: left;
-        position: relative;
-        width: 85px;
-        height: 113px;
-        background-color: #eeece9;
-        border: 1px solid #f0f0f0;
-        border-radius: 1px;
-        overflow: hidden;
-    }
-    
-    .book-h5__cover img {
-        width: 100%;
-        height: 100%;
-        border-radius: 1px;
-    }
-    
-    .book-h5__cover .book-h5__finish {
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-        box-sizing: border-box;
-        font: 10px/10px a;
-        padding: 25px 7px 6px;
-        color: #fff;
-        background: -webkit-linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3));
-    }
-    
-    .book-h5 .book-h5__info {
-        margin-left: 100px;
-        padding-top: 6px;
-        background: #fff;
-    }
-    
-    .book-h5__info .book-h5__title {
-        margin-bottom: 4px;
-        font: 16px/17px a;
-        color: rgba(0, 0, 0, 0.9);
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-    }
-    
-    .book-h5__info .book-h5__author {
-        margin-top: 8px;
-        font: 12px/12px a;
-        color: rgba(0, 0, 0, 0.7);
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-    }
-    
-    .book-h5__info .book-h5__summary {
-        margin: 8px 0 0;
-        height: 2.8em;
-        font: 12px/1.4em a;
-        color: rgba(0, 0, 0, 0.6);
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-    }
-    
-    .book-h5__info .book-h5__wrap {
-        margin-top: 10px;
-        padding-top: 3px;
-        overflow: hidden;
-    }
-    
-    .book-h5__info .book-h5__tag {
-        float: left;
-        margin: -3px 7px 0 0;
-        padding: 3px 6px 2px;
-        max-width: 6em;
-        font: 10px/11px a;
-        color: #53ac7d;
-        border-radius: 3px;
-        border: 1px solid #53ac7d;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-    }
-    
-    .book-h5_no-img__order {
-        float: left;
-        margin-top: 1px;
-        font: 13px/13px a;
-        color: rgba(237, 83, 15, 0.9);
-    }
-    
-    .book-h5_no-img__info {
-        margin-left: 27px;
-    }
-    
-    .book-h5_no-img__title {
-        font: 15px/15px a;
-        color: rgba(0, 0, 0, 0.9);
-        background: #fff;
-    }
-    
-    .book-h5_no-img__author {
-        position: absolute;
-        margin: 1px 0 0 13px;
-        font: 12px/12px a;
-        color: rgba(0, 0, 0, 0.4);
-        background: #fff;
-    }
-    
-    .channel-h5__footer_next {
-        border-top: 1px solid #f0f0f0;
-        overflow: hidden;
-    }
-    
-    .channel-h5__footer_next > a {
-        float: left;
-        width: 50%;
-        padding: 14px;
-        font: 13px/1.3em a;
-        color: rgba(0, 0, 0, 0.9);
-        text-align: center;
-        box-sizing: border-box;
-    }
-    
-    .channel-h5__footer_next > a:first-child {
-        border-right: 1px solid #f0f0f0;
-    }
+.channel-h5 {
+  background: #fff;
+  border-bottom: 10px solid #f5f5f5;
+  position: relative;
+}
+
+.channel-5__header {
+  padding: 15px 13px 14px 13px;
+  border-bottom: 1px solid #f0f0f0;
+}
+
+.chanel-h5__title {
+  position: relative;
+  font: bold 13px/13px a;
+  color: rgba(0, 0, 0, 0.9);
+}
+
+.chanel-h5__title>i {
+  position: absolute;
+  margin: -1px 0 0 5px;
+  padding: 3px 5px 0 5px;
+  font: 9px/9px a;
+  color: #fff;
+  background: #53ac7d;
+  border-radius: 1px;
+}
+
+.tab {
+  position: absolute;
+  right: 13px;
+  top: 9px;
+}
+
+.tab a {
+  position: relative;
+  padding: 16px 7px;
+  font: 12px/12px a;
+  color: #999;
+}
+
+.tab a:after {
+  content: '';
+  position: absolute;
+  top: 16px;
+  bottom: 16px;
+  right: -1px;
+  width: 1px;
+  border-right: 1px solid #ccc;
+}
+
+.tab a:last-child {
+  padding-right: 0px;
+}
+
+.tab a:last-child:after {
+  display: none;
+}
+
+.tab__on {
+  color: #528ae8 !important;
+}
+
+.list-h5 {
+  padding: 0 13px;
+}
+
+.list-h5 li {
+  padding: 17px 0;
+  border-bottom: 1px solid #f0f0f0;
+}
+
+.list-h5 li:last-child {
+  border: none;
+}
+
+.book-h5 {
+  overflow: hidden;
+}
+
+.book-h5__cover {
+  float: left;
+  position: relative;
+  width: 85px;
+  height: 113px;
+  background-color: #eeece9;
+  border: 1px solid #f0f0f0;
+  border-radius: 1px;
+  overflow: hidden;
+}
+
+.book-h5__cover img {
+  width: 100%;
+  height: 100%;
+  border-radius: 1px;
+}
+
+.book-h5__cover .book-h5__finish {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  box-sizing: border-box;
+  font: 10px/10px a;
+  padding: 25px 7px 6px;
+  color: #fff;
+  background: -webkit-linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3));
+}
+
+.book-h5 .book-h5__info {
+  margin-left: 100px;
+  padding-top: 6px;
+  background: #fff;
+}
+
+.book-h5__info .book-h5__title {
+  margin-bottom: 4px;
+  font: 16px/17px a;
+  color: rgba(0, 0, 0, 0.9);
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.book-h5__info .book-h5__author {
+  margin-top: 8px;
+  font: 12px/12px a;
+  color: rgba(0, 0, 0, 0.7);
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.book-h5__info .book-h5__summary {
+  margin: 8px 0 0;
+  height: 2.8em;
+  font: 12px/1.4em a;
+  color: rgba(0, 0, 0, 0.6);
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+}
+
+.book-h5__info .book-h5__wrap {
+  margin-top: 10px;
+  padding-top: 3px;
+  overflow: hidden;
+}
+
+.book-h5__info .book-h5__tag {
+  float: left;
+  margin: -3px 7px 0 0;
+  padding: 3px 6px 2px;
+  max-width: 6em;
+  font: 10px/11px a;
+  color: #53ac7d;
+  border-radius: 3px;
+  border: 1px solid #53ac7d;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.book-h5_no-img__order {
+  float: left;
+  margin-top: 1px;
+  font: 13px/13px a;
+  color: rgba(237, 83, 15, 0.9);
+}
+
+.book-h5_no-img__info {
+  margin-left: 27px;
+}
+
+.book-h5_no-img__title {
+  font: 15px/15px a;
+  color: rgba(0, 0, 0, 0.9);
+  background: #fff;
+}
+
+.book-h5_no-img__author {
+  position: absolute;
+  margin: 1px 0 0 13px;
+  font: 12px/12px a;
+  color: rgba(0, 0, 0, 0.4);
+  background: #fff;
+}
+
+.channel-h5__footer_next {
+  border-top: 1px solid #f0f0f0;
+  overflow: hidden;
+}
+
+.channel-h5__footer_next>a {
+  float: left;
+  width: 50%;
+  padding: 14px;
+  font: 13px/1.3em a;
+  color: rgba(0, 0, 0, 0.9);
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.channel-h5__footer_next>a:first-child {
+  border-right: 1px solid #f0f0f0;
+}
+
 </style>
--- client/views/home.vue
+++ client/views/home.vue
@@ -1,260 +1,262 @@
 <template>
-    <div :style="wrapStyle" @touchend="moveEnd" @touchstart="moveStart" @touchmove="move">
-        <section class="header-tab" id="app_header">
-            <div class="Swipe-tab Swipe-tab_2 f-cb">
-                <a href="javascript:" :class="{SwipeTab__on:isActive1}" @click="tabSwich(0)"></a>
-                <a href="javascript:" :class="{SwipeTab__on:isActive2}" @click="tabSwich(1)"></router-link>
-                    <i :style="underlineStyle"></i>
-            </div>
-            <em class="header-user"></em>
-            <em class="header-checkin"></em>
-        </section>
-        <div :style="contentWrapStyle">
-            <div class="container-wrap" :style="containerWrapStyle">
-                <div class="container-scroll">
-                    <home-search></home-search>
-                    <home-top></home-top>
-                    <home-hot :HotData="HotData"></home-hot>
-                    <home-recommend :RecommendData="RecommendData"></home-recommend>
-                    <home-female :FemaleData="FemaleData"></home-female>
-                    <home-male :MaleData="MaleData"></home-male>
-                    <home-free :FreeData="FreeData"></home-free>
-                    <home-listMore :MaleData="MaleData"></home-listMore>
-                </div>
-            </div>
-            <div class="container-wrap" :style="containerWrapStyle">
-                <div class="container-scroll">
-                    <home-search></home-search>
-                    <home-bookrack></home-bookrack>
-                </div>
-            </div>
+  <div :style="wrapStyle" @touchend="moveEnd" @touchstart="moveStart" @touchmove="move">
+    <section class="header-tab" id="app_header">
+      <div class="Swipe-tab Swipe-tab_2 f-cb">
+        <a href="javascript:" :class="{SwipeTab__on:isActive1}" @click="tabSwich(0)"></a>
+        <a href="javascript:" :class="{SwipeTab__on:isActive2}" @click="tabSwich(1)"></a>
+          <i :style="underlineStyle"></i>
+      </div>
+      <em class="header-user"></em>
+      <em class="header-checkin"></em>
+    </section>
+    <div :style="contentWrapStyle">
+      <div class="container-wrap" :style="containerWrapStyle">
+        <div class="container-scroll">
+          <home-search></home-search>
+          <home-top></home-top>
+          <home-hot :HotData="HotData"></home-hot>
+          <home-recommend :RecommendData="RecommendData"></home-recommend>
+          <home-female :FemaleData="FemaleData"></home-female>
+          <home-male :MaleData="MaleData"></home-male>
+          <home-free :FreeData="FreeData"></home-free>
+          <home-listMore :MaleData="MaleData"></home-listMore>
         </div>
+      </div>
+      <div class="container-wrap" :style="containerWrapStyle">
+        <div class="container-scroll">
+          <home-search></home-search>
+          <home-bookrack></home-bookrack>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-    import search from 'components/home_search';
-    import top from 'components/home_top';
-    import hot from 'components/home_hot';
-    import recommend from 'components/home_recommend';
-    import female from 'components/home_female';
-    import male from 'components/home_male';
-    import free from 'components/home_free';
-    import bookrack from 'components/home_bookrack';
-    import listMore from 'components/home_listMore';
+import search from 'components/home_search';
+import top from 'components/home_top';
+import hot from 'components/home_hot';
+import recommend from 'components/home_recommend';
+import female from 'components/home_female';
+import male from 'components/home_male';
+import free from 'components/home_free';
+import bookrack from 'components/home_bookrack';
+import listMore from 'components/home_listMore';
 
-    export default {
-        components: {
-            'home-search': search,
-            'home-top': top,
-            'home-hot': hot,
-            'home-recommend': recommend,
-            'home-female': female,
-            'home-male': male,
-            'home-free': free,
-            'home-bookrack': bookrack,
-            'home-listMore': listMore
-        },
-        data() {
-            return {
-                HotData: {},
-                RecommendData: {},
-                FemaleData: {},
-                MaleData: {},
-                FreeData: {},
-                position: 0,
-                isActive1: true,
-                isActive2: false,
-                header_tab: 0,
-                coordinateX: 0,
-                distance: 0
-            }
-        },
-        created() {
-            this.$http.get('http://localhost:3000/api/home').then((response) => {
-                let datas = JSON.parse(response.body);
-                this.HotData = datas.items[1].data.data;
-                this.RecommendData = datas.items[2].data.data;
-                this.FemaleData = datas.items[3].data.data;
-                this.MaleData = datas.items[4].data.data;
-                this.FreeData = datas.items[5].data.data;
-            }, response => {
-                console.log(err);
-            });
-        },
-        methods: {
-            tabSwich() {
-                if (arguments[0]) {
-                    this.position = -window.screen.width;
-                    this.isActive1 = false;
-                    this.isActive2 = true;
-                    this.header_tab = 100;
-                } else {
-                    this.position = 0;
-                    this.isActive1 = true;
-                    this.isActive2 = false;
-                    this.header_tab = 0;
-                }
-            },
-            moveStart(event) {
-                this.coordinateX = event.touches[0].clientX;
-            },
-            move(event) {
-                this.distance = event.touches[0].clientX - this.coordinateX;
-                this.header_tab = Math.abs(1 / window.screen.width * this.position) * 100;
-                if (this.distance < 0 && this.position < window.screen.width && this.position > -window.screen.width) {
-                    this.position = this.distance;
-                }
-                if (this.distance > 0 && this.position < 0) {
-                    this.position = -(window.screen.width - this.distance);
-                }
-            },
-            moveEnd(event) {
-                let x = Math.abs(this.position) - (window.screen.width / 2);
-                if (x > 0) {
-                    this.position = -window.screen.width;
-                    this.header_tab = 100;
-                    this.isActive2 = true;
-                    this.isActive1 = false;
-                } else {
-                    this.position = 0;
-                    this.header_tab = 0;
-                    this.isActive1 = true;
-                    this.isActive2 = false;
-                }
-            }
-        },
-        computed: {
-            wrapStyle() {
-                return {
-                    width: `${window.screen.width}px`,
-                    overflow: `hidden`,
-                    height: `100%`
-                }
-            },
-            underlineStyle() {
-                return {
-                    width: `50%`,
-                    transitionDuration: `.3s`,
-                    transform: `translate3d(${this.header_tab}%,0px,0px)`,
-                }
-            },
-            contentWrapStyle() {
-                return {
-                    width: `${window.screen.width * 2}px`,
-                    transitionDuration: `.3s`,
-                    transform: `translate3d(${this.position}px,0px,0px)`,
-                    height: `100%`
-                }
-            },
-            containerWrapStyle() {
-                return {
-                    width: `${window.screen.width}px`,
-                    float: `left`
-                }
-            }
-        }
-    };
+export default {
+  components: {
+    'home-search': search,
+    'home-top': top,
+    'home-hot': hot,
+    'home-recommend': recommend,
+    'home-female': female,
+    'home-male': male,
+    'home-free': free,
+    'home-bookrack': bookrack,
+    'home-listMore': listMore
+  },
+  data() {
+    return {
+      HotData: {},
+      RecommendData: {},
+      FemaleData: {},
+      MaleData: {},
+      FreeData: {},
+      position: 0,
+      isActive1: true,
+      isActive2: false,
+      header_tab: 0,
+      coordinateX: 0,
+      distance: 0
+    }
+  },
+  created() {
+    this.$http.get('http://localhost:3000/api/home').then((response) => {
+      let datas = JSON.parse(response.body);
+      this.HotData = datas.items[1].data.data;
+      this.RecommendData = datas.items[2].data.data;
+      this.FemaleData = datas.items[3].data.data;
+      this.MaleData = datas.items[4].data.data;
+      this.FreeData = datas.items[5].data.data;
+    }, response => {
+      console.log(err);
+    });
+  },
+  methods: {
+    tabSwich() {
+      if (arguments[0]) {
+        this.position = -window.screen.width;
+        this.isActive1 = false;
+        this.isActive2 = true;
+        this.header_tab = 100;
+      } else {
+        this.position = 0;
+        this.isActive1 = true;
+        this.isActive2 = false;
+        this.header_tab = 0;
+      }
+    },
+    moveStart(event) {
+      this.coordinateX = event.touches[0].clientX;
+    },
+    move(event) {
+      this.distance = event.touches[0].clientX - this.coordinateX;
+      this.header_tab = Math.abs(1 / window.screen.width * this.position) * 100;
+      if (this.distance < 0 && this.position < window.screen.width && this.position > -window.screen.width) {
+        this.position = this.distance;
+      }
+      if (this.distance > 0 && this.position < 0) {
+        this.position = -(window.screen.width - this.distance);
+      }
+    },
+    moveEnd(event) {
+      let x = Math.abs(this.position) - (window.screen.width / 2);
+      if (x > 0) {
+        this.position = -window.screen.width;
+        this.header_tab = 100;
+        this.isActive2 = true;
+        this.isActive1 = false;
+      } else {
+        this.position = 0;
+        this.header_tab = 0;
+        this.isActive1 = true;
+        this.isActive2 = false;
+      }
+    }
+  },
+  computed: {
+    wrapStyle() {
+      return {
+        width: `${window.screen.width}px`,
+        overflow: `hidden`,
+        height: `100%`
+      }
+    },
+    underlineStyle() {
+      return {
+        width: `50%`,
+        transitionDuration: `.3s`,
+        transform: `translate3d(${this.header_tab}%,0px,0px)`,
+      }
+    },
+    contentWrapStyle() {
+      return {
+        width: `${window.screen.width * 2}px`,
+        transitionDuration: `.3s`,
+        transform: `translate3d(${this.position}px,0px,0px)`,
+        height: `100%`
+      }
+    },
+    containerWrapStyle() {
+      return {
+        width: `${window.screen.width}px`,
+        float: `left`
+      }
+    }
+  }
+};
+
 </script>
 <style type="text/css" scoped>
-    .header-tab {
-        padding: 0px 90px;
-        text-align: center;
-        line-height: 44px;
-        background: #efeff0;
-        border-bottom: 1px solid #ddd;
-        position: relative;
-    }
-    
-    .header-tab i {
-        bottom: -1px;
-    }
-    
-    .header-tab i:before {
-        content: '';
-        display: block;
-        height: 2px;
-        width: 34px;
-        margin: 0 auto;
-        background: #777;
-    }
-    
-    .header-tab a:before {
-        content: '\4e66\67b6';
-        display: block;
-        margin: 0 auto;
-        width: 2em;
-    }
-    
-    .header-tab a:first-child:before {
-        content: '\4e66\57ce';
-    }
-    
-    .header-user {
-        position: absolute;
-        right: 0px;
-        top: 0px;
-        width: 44px;
-        height: 44px;
-        background: url(../assets/img/user.png) no-repeat center;
-        background-size: 16px;
-    }
-    
-    .header-checkin {
-        position: absolute;
-        left: 0px;
-        top: 0px;
-        width: 44px;
-        height: 44px;
-        background: url(../assets/img/checkin.png) no-repeat center;
-        background-size: 22px;
-    }
-    
-    .Swipe-tab {
-        position: relative;
-        overflow: hidden;
-        z-index: 1;
-    }
-    
-    .Swipe-tab i {
-        position: absolute;
-        left: 0;
-        -webkit-transition: -webkit-transform 0.3s ease-out;
-    }
-    
-    .Swipe-tab a {
-        float: left;
-        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    }
-    
-    .Swipe-tab_2 a {
-        width: 50%;
-    }
-    
-    .f-cb:after {
-        clear: both;
-        content: '';
-        display: block;
-        height: 0;
-        overflow: hidden;
-        visibility: hidden;
-    }
-    
-    .SwipeTab__on {
-        color: #ff6600;
-    }
-    
-    .container-wrap {
-        height: 100%;
-        overflow: hidden;
-        position: relative;
-    }
-    
-    .container-scroll {
-        position: absolute;
-        top: 0px;
-        bottom: 45px;
-        left: 0px;
-        right: 0px;
-        overflow-y: scroll;
-    }
+.header-tab {
+  padding: 0px 90px;
+  text-align: center;
+  line-height: 44px;
+  background: #efeff0;
+  border-bottom: 1px solid #ddd;
+  position: relative;
+}
+
+.header-tab i {
+  bottom: -1px;
+}
+
+.header-tab i:before {
+  content: '';
+  display: block;
+  height: 2px;
+  width: 34px;
+  margin: 0 auto;
+  background: #777;
+}
+
+.header-tab a:before {
+  content: '\4e66\67b6';
+  display: block;
+  margin: 0 auto;
+  width: 2em;
+}
+
+.header-tab a:first-child:before {
+  content: '\4e66\57ce';
+}
+
+.header-user {
+  position: absolute;
+  right: 0px;
+  top: 0px;
+  width: 44px;
+  height: 44px;
+  background: url(../assets/img/user.png) no-repeat center;
+  background-size: 16px;
+}
+
+.header-checkin {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 44px;
+  height: 44px;
+  background: url(../assets/img/checkin.png) no-repeat center;
+  background-size: 22px;
+}
+
+.Swipe-tab {
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+}
+
+.Swipe-tab i {
+  position: absolute;
+  left: 0;
+  -webkit-transition: -webkit-transform 0.3s ease-out;
+}
+
+.Swipe-tab a {
+  float: left;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
+
+.Swipe-tab_2 a {
+  width: 50%;
+}
+
+.f-cb:after {
+  clear: both;
+  content: '';
+  display: block;
+  height: 0;
+  overflow: hidden;
+  visibility: hidden;
+}
+
+.SwipeTab__on {
+  color: #ff6600;
+}
+
+.container-wrap {
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+}
+
+.container-scroll {
+  position: absolute;
+  top: 0px;
+  bottom: 45px;
+  left: 0px;
+  right: 0px;
+  overflow-y: scroll;
+}
+
 </style>
